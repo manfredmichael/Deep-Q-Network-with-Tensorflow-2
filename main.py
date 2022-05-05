576a25cdba042782c13f5067fd38241683c7d07f
manfred michael <koskosanrider@gmail.com>
2022-05-05 22:12:19 +0700

    Add main function

--- main.py
+++ main.py
@@ -2,25 +2,29 @@ from agent import Agent
 import itertools
 import gym
 
-env = gym.make('LunarLander-v2')
+def main():
+    env = gym.make('LunarLander-v2')
 
-obs_shape = env.observation_space.sample().shape
-act_shape = env.action_space.n
+    obs_shape = env.observation_space.sample().shape
+    act_shape = env.action_space.n
 
-agent = Agent(obs_shape, act_shape)
+    agent = Agent(obs_shape, act_shape)
 
-for episode in itertools.count():
-    observation = env.reset()
-    done = False
-    for timestep in itertools.count():
-        env.render()
-        action = agent.take_action(observation)
-        observation_next, reward, done, info = env.step(action)
-        agent.remember(observation, action, observation_next, reward, done)
-        agent.learn()
+    for episode in itertools.count():
+        observation = env.reset()
+        done = False
+        for timestep in itertools.count():
+            env.render()
+            action = agent.take_action(observation)
+            observation_next, reward, done, info = env.step(action)
+            agent.remember(observation, action, observation_next, reward, done)
+            agent.learn()
 
-        observation = observation_next
+            observation = observation_next
 
-        if done:
-            print(agent.get_episode_report())
-            break
+            if done:
+                print(agent.get_episode_report())
+                break
+
+if __name__ == "__main__":
+    main()
